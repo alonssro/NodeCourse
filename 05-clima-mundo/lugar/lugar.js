4af67ad474bf48e348d99f1e8207da3f0bee3349
Alonso Rodríguez <rodriguezalonsod@outlook.com>
2019-02-19 14:12:46 -0600

    Update lugar.js

--- 05-clima-mundo/lugar/lugar.js
+++ 05-clima-mundo/lugar/lugar.js
@@ -3,7 +3,7 @@ const axios = require('axios');
 const getLugarLatLng = async (direccion) => {
     let encodedUrl = encodeURI(direccion);
 
-    let respuesta = await axios.get(`https://maps.googleapis.com/maps/api/geocode/json?address=${encodedUrl}&key=AIzaSyA5mjCwx1TRLuBAjwQw84WE6h5ErSe7Uj8`)
+    let respuesta = await axios.get(`https://maps.googleapis.com/maps/api/geocode/json?address=${encodedUrl}&key=KEY`)
 
     if (respuesta.data.status === 'ZERO_RESULTS') {
         throw new Error(`No hay resultados para la ciudad ${direccion}`)
