0aebf8d833031c8e80ab264ba7062adb173d92ee
Alonso Rodríguez <rodriguezalonsod@outlook.com>
2019-02-19 14:13:09 -0600

    Update clima.js

--- 05-clima-mundo/clima/clima.js
+++ 05-clima-mundo/clima/clima.js
@@ -2,7 +2,7 @@ const axios = require('axios');
 
 const getClima = async (lat, lng) => {
 
-    let resp = await axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=b228afaa1f5d7b1297835430fb255b88`)
+    let resp = await axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=appid`)
 
 
     return resp.data.main.temp;
